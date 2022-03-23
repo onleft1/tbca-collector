e50bed4636d88eedbef05fe6c3d32fb12834496f
onleft1 <onleft@gmail.com>
2022-03-23 23:33:02 +0100

    first dbs

--- collector.py
+++ collector.py
@@ -4,46 +4,44 @@ import pandas as pd
 import csv
 
 pg=1
-#atuald=1
-url = 'http://www.tbca.net.br/base-dados/composicao_alimentos.php?pagina='+str(pg) #+'&atuald='+str(atuald)
-csvfilename="my data.csv"
+mark=1
+url1 = "http://www.tbca.net.br/base-dados/composicao_alimentos.php?pagina=" #+'&atuald='+str(atuald)
+url2 = "http://www.tbca.net.br/base-dados/int_composicao_estatistica.php?cod_produto="
+csvfilename1="my data.csv"
+csvfilename2="alimentos.csv"
+
 df_total = []
 
-while pg<5:
-    html = requests.get(url).content
-    df_list = pd.read_html(html)
-    df_total = df_total + df_list
-    pg = pg + 1
-
-
-#print(type(df))
-df = df_total[0]
-df.to_csv(csvfilename)
-
-#### Converting into a dictionary
-# opening the file in read mode
-file = open(csvfilename, "r")
-replacement = ""
-# using the for loop
-for line in file:
-    line = line.strip()
-    changes = line.replace(",Código,Nome,Nome (Inglês),Nome Científico,Grupo,Marca", "#,Código,Nome,Nome (Inglês),Nome Científico,Grupo,Marca")
-    replacement = replacement + changes + "\n"
-
-
-#print(replacement)
-file.close()
+#### Building the list of codes
+# while pg < 3:
+#     html = requests.get(url1+str(pg)).content #+'&atuald='+str(atuald)
+#     df_list = pd.read_html(html)
+            
+#     if pg == 1:
+#         df = df_list[0]
+#         df.to_csv(csvfilename1, sep='|') #criando primeiro csv (por pagina)
+    
+#     else:
+#         df = df_list[0] #remover cabecalho
+#         #df = df.iloc[1: , :]
+#         df.to_csv(csvfilename1, header= None ,mode ="a", sep="|") #adicionando
+    
+#     pg = pg + 1
 
 # treating the text removing "
-with open(csvfilename, 'r') as infile, \
+with open(csvfilename1, 'r') as infile, \
      open("test.csv", 'w') as outfile:
     data = infile.read()
-    data = data.replace('"', ' ')
+    data = data.replace("|Código|Nome|Nome (Inglês)|Nome Científico|Grupo|Marca", "#|Código|Nome|Nome (Inglês)|Nome Científico|Grupo|Marca")
+    #data = data.replace('"', ' ')
+    #data = data.replace(', ', ',')
+    #data = data.replace(' ,', ',')
+    #data = data.replace(',', ' , ')
     outfile.write(data)
 
-csvfilename="test.csv"
+csvfilename1="test.csv"
 
-# with open(csvfilename, "r") as infile, open("repaired.csv", "w") as outfile:
+# with open(csvfilename1, "r") as infile, open("repaired.csv", "w") as outfile:
 #     reader = csv.reader(infile)
 #     writer = csv.writer(outfile)
 #     conversion = set('"$')
@@ -59,27 +57,35 @@ csvfilename="test.csv"
 
 # doc in https://docs.python.org/3/library/csv.html
 finallist= []
-with open(csvfilename, newline='') as csvfile:
-    spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
+with open(csvfilename1, newline='') as csvfile:
+    spamreader = csv.reader(csvfile, delimiter='|')#, quotechar='|')
     for row in spamreader:
         finallist.append(row)  
-#print(finallist)
-print(finallist[1])
-print(finallist[1][0])
-print(finallist[1][1])
-print(finallist[1][2])
-print(finallist[1][3])
 
-print(len(finallist))
+print()
+print("Foram encontrados " + str(len(finallist)-1) + " alimentos na tabela TBCA.")
+print()
+print("Criando lista de alimentos...")
+#List of codes
+#for i in finallist:
+#    print(finallist[finallist.index(i)][1])
+
+#### Building the list of food using codes
+# for i in finallist:
+#     html = requests.get(url2+str(finallist[finallist.index(i)][1])).content
+#     df_list = pd.read_html(html)
+#     df = df_list[0]
+#     cod = pd.DataFrame({"code": [str(finallist[finallist.index(i)][1])], 
+#         "description": [str(finallist[finallist.index(i)][2])]})
+    
+#     if mark == 1:
+#         cod.to_csv(csvfilename2, header= None , sep="|")
+#         df.to_csv(csvfilename2, header= None ,mode ="a", sep="|")
+#         mark = mark + 1
+    
+#     else:
+#         cod.to_csv(csvfilename2, header= None ,mode ="a", sep="|")
+#         df.to_csv(csvfilename2, header= None ,mode ="a", sep="|")
 
 
-#Creating the Dictionary
-# Dict1 = csv.DictReader(open(csvfile))
-# for row in input_file:
-#     print(row)
-
-# print(type(Dict1))
-
-# for i in Dict1:
-#     print(Dict1[i][Código])
-
+    
